630733c9ad75e72222eb7cb2a9141d5beb2fc059
vvalugonzalez <valu322@gmail.com>
2024-11-07 11:32:14 -0300

    paginacion y filtrado

--- src/app/components/listado-desarrolladores/listado-desarrolladores.component.ts
+++ src/app/components/listado-desarrolladores/listado-desarrolladores.component.ts
@@ -1,42 +1,54 @@
-import { Component } from '@angular/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort, MatSortModule} from '@angular/material/sort';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTooltipModule } from '@angular/material/tooltip';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { RegistrarDesarrolladorDialogComponent } from '../registrar-desarrollador-dialog/registrar-desarrollador-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
 
-export interface PeriodicElement {
+export interface Developers {
   nombre: string;
   email: string;
   rol: string;
   fechaContratacion: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
+const ELEMENT_DATA: Developers[] = [
   { nombre: 'Josefina',email:"joseb31@gmail.com", rol:"Desarrollador", fechaContratacion:'24/03/2018'},
   { nombre: 'Valentina',email:"val7@gmail.com", rol:"Desarrollador", fechaContratacion:'27/01/2024'},
   { nombre: 'Felipe',email:"feli21@gmail.com", rol:"Desarrollador", fechaContratacion:'24/06/2022'},
   { nombre: 'Matías',email:"matias1@gmail.com", rol:"Desarrollador", fechaContratacion:'14/07/2023'},
   { nombre: 'Santiago',email:"santi@gmail.com", rol:"Desarrollador", fechaContratacion:'02/05/2023'},
-  { nombre: 'María', email:"maria1@gmail.com", rol:"Desarrollador", fechaContratacion:'24/03/2018'}
+  { nombre: 'María', email:"maria1@gmail.com", rol:"Desarrollador", fechaContratacion:'24/03/2018'},
+  { nombre: 'Santiago',email:"santi@gmail.com", rol:"Desarrollador", fechaContratacion:'02/05/2023'},
+  { nombre: 'Santiago',email:"santi@gmail.com", rol:"Desarrollador", fechaContratacion:'02/05/2023'}
   
 ];
 
+/**
+ * @title Data table with sorting, pagination, and filtering.
+ */
 @Component({
   selector: 'app-listado-desarrolladores',
-  standalone: true,
-  imports: [MatTableModule, MatFormFieldModule, MatIconModule, MatButtonModule, MatTooltipModule, RegistrarDesarrolladorDialogComponent],
+  styleUrl: './listado-desarrolladores.component.scss',
   templateUrl: './listado-desarrolladores.component.html',
-  styleUrl: './listado-desarrolladores.component.scss'
+  standalone: true,
+  imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule, MatIconModule, RegistrarDesarrolladorDialogComponent, MatTooltipModule, MatButtonModule],
 })
-export class ListadoDesarrolladoresComponent {
-  constructor(public dialog: MatDialog) {}
+export class ListadoDesarrolladoresComponent implements AfterViewInit {
   displayedColumns: string[] = ['nombre', 'email', 'rol', 'fechaContratacion', 'acciones'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  dataSource = new MatTableDataSource<Developers>(ELEMENT_DATA);
 
-   deleteElement(element: any) {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(public dialog: MatDialog) {}
+
+  deleteElement(element: any) {
     const data = this.dataSource.data;
     this.dataSource.data = data.filter(item => item !== element);
   }
@@ -48,5 +60,19 @@ export class ListadoDesarrolladoresComponent {
         this.dataSource.data = [...this.dataSource.data, result];
       }
     });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
